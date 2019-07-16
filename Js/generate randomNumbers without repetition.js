8a6d1d42cb27b466ce64d88d29144febc0e5abdc
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-07-17 00:20:17 +0600

    random generator

---
+++ Js/generate randomNumbers without repetition.js
@@ -0,0 +1,15 @@
+var list = [];
+var counter = 0;
+var highestNumber = 60;
+var lowestNumber = 1;
+var totalNumbersWanted = 30;
+
+while (list.length < totalNumbersWanted) {
+    var number = Math.floor(Math.random() * (highestNumber - lowestNumber + 1)) + lowestNumber;
+
+    if (!list.includes(number)) {
+        list.push(number);
+    }
+}
+
+console.log(list);
