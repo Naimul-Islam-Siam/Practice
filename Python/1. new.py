d4e47bf2211a5e7f32dbbc0409b6ec07cb8c7e55
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-11-20 20:51:58 +0600

    py

---
+++ Python/1. new.py
@@ -0,0 +1,5 @@
+name = input("What is your name?") #scanf
+print("My name is " + name)
+print(bin(5)) #binary of 5
+print(int("0b101", 2)) #first argument is a base 2 number, convert it to an int
+print(2 ** 3) #same as 2^3
