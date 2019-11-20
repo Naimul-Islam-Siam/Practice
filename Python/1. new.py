5eb9b78ef4cd4d96d65d1773818bbe55e67f14f5
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-11-20 21:19:15 +0600

    py

--- Python/1. new.py
+++ Python/1. new.py
@@ -3,3 +3,26 @@ print("My name is " + name)
 print(bin(5)) #binary of 5
 print(int("0b101", 2)) #first argument is a base 2 number, convert it to an int
 print(2 ** 3) #same as 2^3
+a,b,c = 1,2,3
+print(a) #1
+print(b) #2
+print(c) #3
+
+# long string ''' allows string to occupy multiple lines
+long_string = '''
+WOW
+O O
+---
+'''
+print(long_string)
+
+#Escape Sequence
+print("It\'s \"kind of\" funny")
+# \t = tab
+# \n = new line
+
+
+#Formatted String
+age = 55
+name = "Johnny"
+print(f"{name} is {age} years old.") #f must be included
