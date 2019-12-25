d0540a773ae6f7325fd118174016f79c2d8d3dd3
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-12-25 23:44:50 +0600

    react-router

--- React/react-router/src/App.js
+++ React/react-router/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// ====================================================
+// IF YOU ARE LOOKING FOR THE 'fake' ROUTING EXAMPLE...
+// I MOVED THE CODE INTO FakeRouting.js
+// ====================================================
+import React, { Component } from "react";
+import About from "./About";
+import Dog from "./Dog";
+import Contact from "./Contact";
+import "./App.css";
+import { Route, Switch, NavLink } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Hater = () => <h1>I ABSOLUTELY HATE DOGS!</h1>;
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <nav className="App-nav">
+          {/* NavLink is better than Link for nav as NavLink can have activeClassName */}
+          <NavLink exact activeClassName="active-link" to="/">About</NavLink>
+          <NavLink exact activeClassName="active-link" to="/dog">Dog</NavLink>
+          <NavLink exact activeClassName="active-link" to="/contact">Contact</NavLink>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={About} />
+          {/* if Component has props it's better to use "render" instead of "component"  */}
+          {/* "render" only renders and reuse the existing Component whenever it's clicked */}
+          {/* "component" not just renders but also instantiate the Component and goes through the whole lifecycle every time*/}
+          <Route exact path="/dog" render={() => <Dog name="Muffin" />} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
