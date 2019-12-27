5d8aa655dd9b1c8ceefe6f3b04be619de7d03497
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-12-27 12:44:24 +0600

    router

--- React/react-router/src/About.js
+++ React/react-router/src/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Message from './Message';
 
 class About extends Component {
   render() {
@@ -6,6 +7,10 @@ class About extends Component {
       <div className='About'>
         <h1>About!!!</h1>
         <p>This is the about page...</p>
+
+        <Message>
+          <h1>I'm about page</h1>
+        </Message>
       </div>
     );
   }
--- React/react-router/src/Contact.js
+++ React/react-router/src/Contact.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Message from "./Message";
 
 class Contact extends Component {
   render() {
     return (
       <div className='Contact'>
         <h1>Contact!!!</h1>
+        <Message>
+          <h3>It's Contact Page</h3>
+        </Message>
         <p>PLEASE DO NOT CONTACT US EVER</p>
       </div>
     );
