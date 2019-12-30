d7dd96cb744748bf7eff5abb88bb9e68d53ccc3d
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-12-30 16:47:22 +0600

    fix

--- React/router-exercise/src/NavBar.js
+++ React/router-exercise/src/NavBar.js
@@ -5,7 +5,7 @@ export default class NavBar extends Component {
    render() {
       const dogLink = this.props.dogs.map(dog =>
          <li className="nav-item" key={dog.name}>
-            <NavLink to={`/dogs/${dog.name}`} className="nav-link">{dog.name}</NavLink>
+            <NavLink exact to={`/dogs/${dog.name}`} className="nav-link">{dog.name}</NavLink>
          </li>
       );
       return (
@@ -24,9 +24,6 @@ export default class NavBar extends Component {
 
             <div className="collapse navbar-collapse" id="navbarNav">
                <ul className="navbar-nav">
-                  <li className="nav-item">
-                     <NavLink to="/dogs" className="nav-link">Home</NavLink>
-                  </li>
                   {dogLink}
                </ul>
             </div>
