fe19df4a7b69116cbf86c424b3cdab9589b79d52
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-01-08 22:13:46 +0600

    useEffect and new

--- React/hooks/src/App.js
+++ React/hooks/src/App.js
@@ -4,13 +4,19 @@ import './App.css';
 import CounterClass from './CounterClass';
 import CounterHooks from './CounterHooks';
 import Toggler from './Toggler';
+import SWMovies from './SWMovies';
 
 function App() {
   return (
     <div className="App">
+      <SWMovies />
+      <p>-------------------------</p>
       <CounterClass />
+      <p>-------------------------</p>
       <CounterHooks />
+      <p>-------------------------</p>
       <Toggler />
+      <p>-------------------------</p>
     </div>
   );
 }
