435c3bdceb43495c134506831082d6164fb6d622
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-06-10 23:29:58 +0600

    Py new start

---
+++ Python/escape sequence.py
@@ -0,0 +1,8 @@
+weather = "It's \"kind of\" funny. \n Hope you have a great day"
+
+john = 'He\'s a good boy'
+
+print(weather)
+print(john)
+
+print("We use \ as an escape sequence.")
---
+++ Python/type conversion.py
@@ -0,0 +1,13 @@
+var1 = "100"
+var2 = 100
+
+print(type(var1))
+print(type(var2))
+
+var3 = int(var1)
+var4 = str(var2)
+var5 = str(int(var1))
+
+print(type(var3))
+print(type(var4))
+print(type(var5))
