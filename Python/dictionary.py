2569fe2ce83423ca19e2d7d5b037768289a50c92
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-06-11 23:23:54 +0600

    Dictionary

---
+++ Python/dictionary.py
@@ -0,0 +1,23 @@
+user = {
+   'country': 'uk',
+   'package': 'standard',
+   'age': 22
+}
+
+print(user.get('age', 55)) # 22 . As a key named 'age' exists in user dictionary, it will use the default value
+
+user = {
+   'country': 'uk',
+   'package': 'standard'
+}
+
+print(user.get('age', 55)) # 55 . As a key named 'age' doesn't exist in user dictionary, it will use the value later assigned
+
+user2 = user.copy()
+
+print(user.clear()) # None
+print(user2) # {'country': 'uk', 'package': 'standard'}
+
+user2.update({'country': 'esp'})
+
+print(user2) # {'country': 'esp', 'package': 'standard'}
