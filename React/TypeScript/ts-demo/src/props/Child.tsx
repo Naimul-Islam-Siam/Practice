0df25099657e32fc280f38f9807d0c98eaae34b6
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-06-20 19:03:14 +0600

    Props

--- React/TypeScript/ts-demo/src/props/Child.tsx
+++ React/TypeScript/ts-demo/src/props/Child.tsx
@@ -1,3 +1,19 @@
-export const Child = () => {
-   return <div>Hi from Children!</div>
+interface ChildProps {
+   color: string;
+   onClick: ()=> void;
+};
+
+export const Child = ({color, onClick}: ChildProps) => {
+   return (
+      <div>
+         {color}
+         <button onClick={onClick}>Click Me!</button>
+      </div>
+   );
+};
+
+// Another way of defining a FunctionalComponent in TypeScript
+// This is the more explicit way
+export const ChildAsFC: React.FC<ChildProps> = ({color}) => {
+   return <div>{color}</div>
 };
