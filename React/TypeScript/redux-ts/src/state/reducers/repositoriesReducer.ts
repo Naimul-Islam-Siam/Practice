6cfab5cf216babdced58ef0a8c3b1ea7760b3dfe
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-06-22 22:55:03 +0600

    Folder Structure

--- React/TypeScript/redux-ts/src/state/reducers/repositoriesReducer.ts
+++ React/TypeScript/redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -1,35 +1,22 @@
+import {Action} from './../actions/index';
+import {ActionType} from './../action-types/index';
+
 interface RepositoriesState {
    loading: boolean;
    error: string | null;
    data: string[];
 };
 
-interface SearchRepositories {
-   type: 'search_repositories'
-};
 
-interface SearchRepositoriesSuccess {
-   type: 'search_repositories_success';
-   payload: string[];
-};
-
-interface SearchRepositoriesError {
-   type: 'search_repositories_error';
-   payload: string;
-};
-
-const reducer = (
-   state: RepositoriesState, 
-   action: SearchRepositories | SearchRepositoriesSuccess | SearchRepositoriesError
-   ): RepositoriesState => {
+const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
    switch (action.type) {
-      case 'search_repositories':
+      case ActionType.SEARCH_REPOSITORIES:
          return {loading: true, error: null, data: []};
 
-      case 'search_repositories_success':
+      case ActionType.SEARCH_REPOSITORIES_SUCCESS:
          return {loading: false, error: null, data: action.payload};
 
-      case 'search_repositories_error':
+      case ActionType.SEARCH_REPOSITORIES_ERROR:
          return {loading: false, error: action.payload, data: []};
 
       default:
