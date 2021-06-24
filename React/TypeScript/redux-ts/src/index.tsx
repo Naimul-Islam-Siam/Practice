9a569a4bb82dbb4898b7ecfb1ceea468975aa45a
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-06-24 14:24:29 +0600

    Dispatch Actions

---
+++ React/TypeScript/redux-ts/src/index.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+const Index = () => {
+   return (
+      <div>
+         <App />
+      </div>
+   );
+};
+
+ReactDOM.render(<Index />, document.querySelector("#root"));
--- React/TypeScript/redux-ts/src/state/reducers/repositoriesReducer.ts
+++ React/TypeScript/redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -7,8 +7,14 @@ interface RepositoriesState {
    data: string[];
 };
 
+const initialState = {
+   loading: false,
+   error: null,
+   data: []
+};
 
-const repositoriesReducer = (state: RepositoriesState, action: Action): RepositoriesState => {
+
+const repositoriesReducer = (state: RepositoriesState = initialState, action: Action): RepositoriesState => {
    switch (action.type) {
       case ActionType.SEARCH_REPOSITORIES:
          return {loading: true, error: null, data: []};
