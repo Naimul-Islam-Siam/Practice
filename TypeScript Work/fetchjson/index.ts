51065f5f45f313e74c9edd360167efda09ab6d3c
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-07-23 23:47:05 +0600

    fixed

--- TypeScript Work/fetchjson/index.ts
+++ TypeScript Work/fetchjson/index.ts
@@ -11,9 +11,9 @@ interface Todo {
 axios.get(url).then(res => {
    const todo = res.data as Todo;
 
-   const {id, title, completed} = todo;
+   const { id, title, completed } = todo;
 
-   logTodo(id, title, completed);   
+   logTodo(id, title, completed);
 });
 
 const logTodo = (id: number, title: string, completed: boolean) => {
