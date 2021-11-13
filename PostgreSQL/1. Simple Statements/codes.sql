afc458d854591f4a70d27a7d83b75299f794d04b
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-11-13 20:51:40 +0600

    Exercise and string operators and functions

--- PostgreSQL/1. Simple Statements/codes.sql
+++ PostgreSQL/1. Simple Statements/codes.sql
@@ -26,3 +26,49 @@ SELECT name, name FROM cities;
 
 -- Transform/Process data before pulling it out
 SELECT name, population/area AS population_density FROM cities;
+
+
+
+-- Exercise
+CREATE TABLE phones (
+  name VARCHAR(50),
+  manufacturer VARCHAR(50),
+  price INTEGER,
+  units_sold INTEGER
+);
+
+
+INSERT INTO phones (name, manufacturer, price, units_sold)
+VALUES
+  ('N1280', 'Nokia', 199, 1925),
+  ('Iphone 4', 'Apple', 399, 9436),
+  ('Galaxy S', 'Samsung', 299, 2359),
+  ('S5620 Monte', 'Samsung', 250, 2385),
+  ('N8', 'Nokia', 150, 7543);
+
+
+SELECT name, price*units_sold AS revenue FROM phones;
+
+
+
+-- String operators and functions
+SELECT name || ', ' || country as location FROM cities;
+-- or
+SELECT CONCAT(name, ', ', country) as location FROM cities;
+
+-- Output:
+-- location
+-- Tokyo, Japan
+-- Delhi, India
+-- Shanghai, China
+-- Sao Paulo, Brazil
+
+
+SELECT UPPER(CONCAT(name, ', ', country)) as location FROM cities;
+
+-- Output:
+-- location
+-- TOKYO, JAPAN
+-- DELHI, INDIA
+-- SHANGHAI, CHINA
+-- SAO PAULO, BRAZIL
