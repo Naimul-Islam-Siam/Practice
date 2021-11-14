e7ac899e19e5e44f68b72e890d370b5894d409dc
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-11-14 11:52:18 +0600

    Foreign Key and Constraints

---
+++ PostgreSQL/3. Working with Tables/codes.sql
@@ -0,0 +1,88 @@
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+  username VARCHAR(50)
+);
+
+
+INSERT INTO users (username)
+VALUES
+  ('monahan93'),
+  ('pfeffer'),
+  ('99stroman'),
+  ('sim3onis');
+
+
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  url VARCHAR(200),
+  user_id INTEGER REFERENCES users(id)
+);
+
+
+INSERT INTO photos (url, user_id)
+VALUES
+  ('http://img1.jpg', 4),
+  ('http://img2.jpg', 4),
+  ('http://img3.jpg', 1),
+  ('http://img4.jpg', 2);
+
+
+SELECT * FROM photos WHERE user_id = 4;
+
+SELECT * FROM photos JOIN users on users.id = photos.user_id;
+
+SELECT url, username FROM photos JOIN users on users.id = photos.user_id;
+
+
+
+-- Exercise:
+
+CREATE TABLE boats (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50)
+);
+
+INSERT INTO boats (name)
+VALUES
+  ('Rogue Wave'),
+  ('Harbor Master');
+
+CREATE TABLE crew_members (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  boat_id INTEGER REFERENCES boats(id)
+);
+
+INSERT INTO crew_members (name, boat_id)
+VALUES
+  ('Alex', 1),
+  ('Lucia', 1),
+  ('Ari', 2);
+
+
+SELECT * FROM crew_members WHERE boat_id = 1;
+
+
+-- FOREIGN KEY CONSTRAINTS around INSERT:
+
+-- when a photo is insert to a user that doesn't exist: ERROR
+INSERT INTO photos (url, user_id)
+VALUES
+  ('http://img.jpg', 999),
+
+
+-- when a photo is insert that is not associated/tied to any user
+INSERT INTO photos (url, user_id)
+VALUES
+  ('http://img.jpg', NULL),
+
+
+-- CONSTRAINTS around deletion:
+
+-- What happens when we delete a user that is referencing a photo/photos:
+
+-- ON DELETE RESTRICT -> Will not allow the user to be deleted
+-- ON DELETE NO ACTION -> kind of same as restrict but some slight differences
+-- ON DELETE CASCADE -> Will delete the user and all the photos it was referencing
+-- ON DELETE SET NULL -> Delete the user and set the user_id of photos associated to NULL
+-- ON DELET SET DEFAULT -> User will be deleted and user_id will be set to a default value, if one is provided 
