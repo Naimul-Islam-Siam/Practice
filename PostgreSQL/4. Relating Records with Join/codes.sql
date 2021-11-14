1a7875d8d117c1964639e48774b142a9ad98cc26
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-11-14 21:45:19 +0600

    Different types of JOIN

--- PostgreSQL/4. Relating Records with Join/codes.sql
+++ PostgreSQL/4. Relating Records with Join/codes.sql
@@ -20,3 +20,23 @@ SELECT title, name FROM books JOIN authors on books.author_id = authors.id;
 -- Provide context if column names collide because of joining
 SELECT comments.id AS comment_id, photos.id AS photo_id FROM comments JOIN photos on comments.photo_id = photos.id;
 -- otherwise id columns would collide
+
+
+
+-- Different JOIN
+
+-- 1. INNER JOIN (when we use just JOIN it is INNER JOIN by default)
+-- Whatever doesn't match gets removed: Intersection of two TABLEs
+
+-- 2. LEFT JOIN
+-- Take everything from the Left Table, even if it doesn't match with the right table. 
+-- The contents of right table will get a value of NULL if needed
+
+-- 3. RIGHT JOIN
+-- Take everything from the Right Table, even if it doesn't match with the left table. 
+-- The contents of left table will get a value of NULL if needed
+
+-- 4. FULL JOIN
+-- Merge everything whether they match or not
+-- Rows and Columns that doesn't match might get NULL value if needed
+-- All possible rows/records considering both tables of JOIN must be included 
