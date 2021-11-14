a962d5d33909b75c7b3cae16f23ff2f38b7a5268
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-11-15 02:25:44 +0600

    Exercise

--- PostgreSQL/5. Aggregation of Records/codes.sql
+++ PostgreSQL/5. Aggregation of Records/codes.sql
@@ -49,3 +49,36 @@ SELECT authors.name, COUNT(books.author_id)
 FROM books
 JOIN authors ON authors.id = books.author_id
 GROUP BY authors.name;
+
+
+
+-- order: FROM -> JOIN -> WHERE -> GROUP BY -> HAVING
+
+-- Find the number of comments for each photo where the photo_id is less then 3
+SELECT photo_id, COUNT(contents)
+FROM comments
+WHERE photo_id < 3
+GROUP BY photo_id;
+
+-- Find the number of comments for each photo where the photo_id is less then 3 and the photo has at least 2 comments
+SELECT photo_id, COUNT(contents)
+FROM comments
+WHERE photo_id < 3
+GROUP BY photo_id
+HAVING COUNT(contents) > 2;
+
+
+
+-- Find the users who commented on the first 2 photos and has commented at least 2 comments in total
+SELECT user_id, COUNT(contents)
+FROM comments
+WHERE photo_id < 3
+GROUP BY user_id
+HAVING COUNT(contents) > 2
+
+
+
+SELECT manufacturer, SUM(price * units_sold)
+FROM phones
+GROUP BY manufacturer
+HAVING SUM(price * units_sold) > 2000000;
