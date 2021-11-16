c28631818d55455cfcfc65ea39510b16513ee83e
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-11-16 13:23:32 +0600

    Union, Intersection, Except

---
+++ PostgreSQL/8. Union, Intersection and Except/codes.sql
@@ -0,0 +1,78 @@
+-- 4 most expensive products and 4 highest price/weight products
+(
+  SELECT *
+  FROM products
+  ORDER BY price DESC
+  LIMIT 4
+)
+UNION
+(
+  SELECT *
+  FROM products
+  ORDER BY price / weight DESC
+  LIMIT 4
+)
+
+-----------------------------------------
+
+(
+  SELECT *
+  FROM products
+  ORDER BY price DESC
+  LIMIT 4
+)
+INTERSECT
+(
+  SELECT *
+  FROM products
+  ORDER BY price / weight DESC
+  LIMIT 4
+)
+
+-- UNION vs UNION ALL: union will show common results once and remove duplicates, UNION ALL will show all instances (duplicates) of common results
+-- {2, 3, 4, 5} UNION {2, 7, 8, 10} = {2, 3, 4, 5, 7, 8, 10}
+-- {2, 3, 4, 5} UNION ALL {2, 7, 8, 10} = {2, 3, 4, 5, 2, 7, 8, 10}
+
+
+-- INTERSECT vs INTERSECT ALL: INTERSECT will show common results only and once even if duplicates are there,
+-- INTERSECT ALL will show common results multiple time if duplicates are present
+-- {2, 3, 4, 5} INTERSECT {2, 7, 8, 10} = {2}
+-- {2, 3, 4, 5} INTERSECT ALL {2, 7, 8, 10} = {2}
+-- {2, 3, 4, 5} INTERSECT {2, 4, 7, 8, 10} = {2, 4}
+-- {2, 3, 4, 5} INTERSECT ALL {2, 4, 7, 8, 10} = {2, 4}
+-- {2, 3, 3, 4, 5} INTERSECT {2, 3, 4, 3, 7, 8, 10} = {2, 3, 4}
+-- {2, 3, 3, 4, 5} INTERSECT ALL {2, 3, 4, 3, 7, 8, 10} = {2, 3, 3, 4}
+
+
+
+(
+  SELECT *
+  FROM products
+  ORDER BY price DESC
+  LIMIT 4
+)
+EXCEPT
+(
+  SELECT *
+  FROM products
+  ORDER BY price / weight DESC
+  LIMIT 4
+)
+
+-- the above query and the query below is different:
+
+(
+  SELECT *
+  FROM products
+  ORDER BY price / weight DESC
+  LIMIT 4
+)
+EXCEPT
+(
+  SELECT *
+  FROM products
+  ORDER BY price DESC
+  LIMIT 4
+)
+
+-- incase of EXCEPT: order of table matters
