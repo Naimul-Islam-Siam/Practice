fa39d5b0c4265a8df34beda2b0cfc2970dd01d01
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-11-17 23:51:37 +0600

    SubQueries first rule

---
+++ PostgreSQL/9. Assembling Queries with SubQueries/codes.sql
@@ -0,0 +1,13 @@
+-- print the name and price of all products that are more expensive than all products in 'Toys' department
+SELECT name, price 
+FROM products
+WHERE price > (
+  SELECT MAX(price) FROM products WHERE department = 'Toys'
+);
+
+
+-- Exercise:
+SELECT name, price, price / (SELECT MAX(price) FROM phones) AS price_ratio FROM phones;
+
+-- Rules for SubQuery:
+-- 1. If a subquery returns a single result, it can be used in SELECT -> SELECT id, (SELECT MAX(price) FROM products) FROM db;
