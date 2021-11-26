3742d06a6a115368993563c0dda6562c0f375aba
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-11-26 23:54:59 +0600

    subqueries from

--- PostgreSQL/9. Assembling Queries with SubQueries/codes.sql
+++ PostgreSQL/9. Assembling Queries with SubQueries/codes.sql
@@ -9,5 +9,13 @@ WHERE price > (
 -- Exercise:
 SELECT name, price, price / (SELECT MAX(price) FROM phones) AS price_ratio FROM phones;
 
+
+SELECT name, price_weight_ratio
+FROM (
+  SELECT name, price / weight AS price_weight_ratio FROM products
+) AS p
+WHERE price_weight_ratio > 5;
+
+
 -- Rules for SubQuery:
 -- 1. If a subquery returns a single result, it can be used in SELECT -> SELECT id, (SELECT MAX(price) FROM products) FROM db;
