988976e9372c236f36fd26657c36e2203fee8e6b
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-12-20 23:52:52 +0600

    types

--- Js/Deep JavaScript Foundations/1 Introduction.js
+++ Js/Deep JavaScript Foundations/1 Introduction.js
@@ -1,3 +1,6 @@
+// The JS doesn't dictate the language
+// The specs of the language does ✅
+
 var x = 5;
 
 x++; // 5
@@ -7,3 +10,20 @@ x; // 6
 ++x; // 7
 
 x; // 7
+
+
+// 3 pillars of JavaScript
+// 1. Types:
+      // Coercion
+      // Equality
+      // Primitive Types
+      // Abstract Operations
+// 2. Scope:
+      // Hoisting
+      // Closures
+      // Modules
+      // Nested Scope
+// 3. Objects:
+      // this
+      // Class
+      // Prototypes
