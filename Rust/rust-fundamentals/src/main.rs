8d0fc5df0fe2888d2ba334c64270a1d6f73070c8
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-01-03 23:22:55 +0600

    variables

--- Rust/rust-fundamentals/src/main.rs
+++ Rust/rust-fundamentals/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let name = "John";
+    let mut relation_status = "single";
+
+    // mut must be used before the variable name if the variable is intended to be mutable
+    // in Rust, by defaul a variable is not mutable
+    relation_status = "not_single";
 }
---
+++ Rust/rust-fundamentals/src/variables.rs
@@ -0,0 +1,8 @@
+fn main() {
+   let name = "John";
+   let mut relation_status = "single";
+
+   // mut must be used before the variable name if the variable is intended to be mutable
+   // in Rust, by defaul a variable is not mutable
+   relation_status = "not_single";
+}
