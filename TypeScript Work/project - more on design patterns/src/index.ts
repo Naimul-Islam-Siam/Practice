22cb9db2bb252b34976ec626545181add054e59f
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-02-06 00:19:52 +0600

    sort characters

--- TypeScript Work/project - more on design patterns/src/index.ts
+++ TypeScript Work/project - more on design patterns/src/index.ts
@@ -1,8 +1,14 @@
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 import { Sorter } from "./Sorter";
 
 const numbersCollection = new NumbersCollection([10, 2, 5, 3, 7]);
+const charactersCollection = new CharactersCollection("Mobile");
 
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(sorter.collection);
+const numberSorter = new Sorter(numbersCollection);
+numberSorter.sort();
+console.log(numberSorter.collection);
+
+const characterSorter = new Sorter(charactersCollection);
+characterSorter.sort();
+console.log(characterSorter.collection);
