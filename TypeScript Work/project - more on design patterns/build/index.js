6a9227dfbf97ec1a034efa648b9c54172e90c672
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-02-06 00:03:30 +0600

    new gitignore

--- TypeScript Work/project - more on design patterns/build/index.js
+++ TypeScript Work/project - more on design patterns/build/index.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log("Hi There!");
+Object.defineProperty(exports, "__esModule", { value: true });
+var NumbersCollection_1 = require("./NumbersCollection");
+var Sorter_1 = require("./Sorter");
+var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 2, 5, 3, 7]);
+var sorter = new Sorter_1.Sorter(numbersCollection);
+sorter.sort();
+console.log(sorter.collection);
