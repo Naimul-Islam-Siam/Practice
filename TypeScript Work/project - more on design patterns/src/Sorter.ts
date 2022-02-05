8ffbf27a95a74c628e81882df05240753fb39846
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-02-06 05:22:38 +0600

    Refactor to be an Abstract Class

--- TypeScript Work/project - more on design patterns/src/Sorter.ts
+++ TypeScript Work/project - more on design patterns/src/Sorter.ts
@@ -1,21 +1,26 @@
-interface Sortable {
-   length: number;
-   compare(leftIndex: number, rightIndex: number): boolean;
-   swap(leftIndex: number, rightIndex: number): void;
-};
-
-export class Sorter {
-   constructor(public collection: Sortable) {}
+export abstract class Sorter {
+   // child classes must have these methods to use parent's methods
+   abstract compare(leftIndex: number, rightIndex: number): boolean;
+   abstract swap(leftIndex: number, rightIndex: number): void;
+   abstract length: number;
 
    sort(): void {
-      const { length } = this.collection; 
+      const { length } = this; 
       
       for(let i = 0; i < length; i++) {
          for(let j = 0; j < length - i - 1; j++) {
-            if(this.collection.compare(j, j+1)) {
-               this.collection.swap(j, j+1);
+            if(this.compare(j, j+1)) {
+               this.swap(j, j+1);
             }
          }
       }
    }
 };
+
+/*
+   Abstract Class:
+      - An abstract class can't be used to create an object directly
+      - Can only be used as a parent class
+      - Can contain real implementations for some methods
+      - Implemented methods can refer to other methods that don't actually exist yet
+*/
