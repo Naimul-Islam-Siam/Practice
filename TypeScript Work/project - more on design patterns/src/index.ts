10d4022f6364790fa99d7c72dec1e2e4342cc747
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-02-06 04:49:27 +0600

    sort LinkedList

--- TypeScript Work/project - more on design patterns/src/index.ts
+++ TypeScript Work/project - more on design patterns/src/index.ts
@@ -1,9 +1,16 @@
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 import { Sorter } from "./Sorter";
 
 const numbersCollection = new NumbersCollection([10, 2, 5, 3, 7]);
 const charactersCollection = new CharactersCollection("Mobile");
+const linkedList = new LinkedList();
+linkedList.add(10);
+linkedList.add(200);
+linkedList.add(-30);
+linkedList.add(17);
+
 
 const numberSorter = new Sorter(numbersCollection);
 numberSorter.sort();
@@ -12,3 +19,7 @@ console.log(numberSorter.collection);
 const characterSorter = new Sorter(charactersCollection);
 characterSorter.sort();
 console.log(characterSorter.collection);
+
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
