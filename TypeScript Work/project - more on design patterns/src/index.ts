37e600aa32774a26035c2caaf1c44bfa053bf9d8
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-02-06 00:02:17 +0600

    refactor to be a general purpose sorter

--- TypeScript Work/project - more on design patterns/src/index.ts
+++ TypeScript Work/project - more on design patterns/src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hi There!");
+import { NumbersCollection } from "./NumbersCollection";
+import { Sorter } from "./Sorter";
+
+const numbersCollection = new NumbersCollection([10, 2, 5, 3, 7]);
+
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(sorter.collection);
