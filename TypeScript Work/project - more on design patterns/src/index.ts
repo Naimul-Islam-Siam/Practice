15b4b63373b1a4ae4f8d1193bbd492ee6ca1a8fa
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-02-06 05:23:31 +0600

    refactored

--- TypeScript Work/project - more on design patterns/src/index.ts
+++ TypeScript Work/project - more on design patterns/src/index.ts
@@ -1,25 +1,20 @@
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
-import { Sorter } from "./Sorter";
 
 const numbersCollection = new NumbersCollection([10, 2, 5, 3, 7]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
+
 const charactersCollection = new CharactersCollection("Mobile");
+charactersCollection.sort();
+console.log(charactersCollection.data);
+
 const linkedList = new LinkedList();
 linkedList.add(10);
 linkedList.add(200);
 linkedList.add(-30);
 linkedList.add(17);
 
-
-const numberSorter = new Sorter(numbersCollection);
-numberSorter.sort();
-console.log(numberSorter.collection);
-
-const characterSorter = new Sorter(charactersCollection);
-characterSorter.sort();
-console.log(characterSorter.collection);
-
-const linkedListSorter = new Sorter(linkedList);
-linkedListSorter.sort();
+linkedList.sort();
 linkedList.print();
