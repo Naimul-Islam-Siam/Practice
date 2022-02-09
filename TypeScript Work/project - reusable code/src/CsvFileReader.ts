5f8eebe1078fa6ce422a7e52fde062a66f49732a
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-02-09 23:51:31 +0600

    add type for each elements of row array

--- TypeScript Work/project - reusable code/src/CsvFileReader.ts
+++ TypeScript Work/project - reusable code/src/CsvFileReader.ts
@@ -1,7 +1,11 @@
 import fs from "fs";
+import { dateStringToDate } from "./utils";
+import { MatchResults } from "./MatchResults";
+
+export type MatchData = [Date, string, string, number, number, MatchResults, string]; // tuple
 
 export class CsvFileReader {
-   data: string[][] = [];
+   data: MatchData[] = [];
 
    constructor(public filename: string) {}
 
@@ -12,6 +16,17 @@ export class CsvFileReader {
       .split("\n")
       .map((row: string): string[] => { 
          return row.split(",")
+      })
+      .map((row: string[]): MatchData => {
+         return [
+            dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5] as MatchResults, // type assertion
+            row[6] 
+         ];
       });
    }
 };
