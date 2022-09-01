ce78afb6b3c97a2c45833a2861c0c32e76669ec0
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-09-01 19:49:34 +0600

    optional properties

---
+++ TypeScript/Colt Steele/optional properties - type.ts
@@ -0,0 +1,8 @@
+type Points = {
+   x: number,
+   y: number,
+   z?: number // optional property
+};
+
+const TwoDimensionalPoint: Points = { x: 20, y: 10 };
+const ThreeDimensionalPoint: Points = { x: 10, y: 15, z: 27 };
