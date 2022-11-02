36e183c5804760fd0d9f6a34ff3195071fb8d264
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-11-02 18:09:16 +0600

    all page fixed nav added in app layout

--- Nextjs/nextjs-13/crud-fireship/app/layout.tsx
+++ Nextjs/nextjs-13/crud-fireship/app/layout.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-head-element */
+import Link from 'next/link';
 import './globals.css';
 
 export default function RootLayout({
@@ -8,8 +9,15 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head></head>
-      <body>{children}</body>
+      <body>
+        <main>
+          <nav>
+            <Link href="/">Home</Link>
+            <Link href="/notes">Notes</Link>
+          </nav>
+          {children}
+        </main>
+      </body>
     </html>
   );
 };
