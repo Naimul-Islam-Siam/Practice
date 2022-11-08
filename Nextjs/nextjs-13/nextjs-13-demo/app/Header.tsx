e0709d4ed9461c110a38c0aa939c1be27c90ee8f
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-11-09 02:30:10 +0600

    Header component and add that to layout

---
+++ Nextjs/nextjs-13/nextjs-13-demo/app/Header.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Header() {
+   return (
+      <header className="bg-black p-5 mb-5 flex justify-start gap-6">
+         <Link href="/" className="text-white">Home</Link>
+         <Link href="/todos" className="text-white">Todo</Link>
+      </header>
+   );
+};
