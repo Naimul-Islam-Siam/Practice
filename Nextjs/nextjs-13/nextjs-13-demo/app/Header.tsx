b7090fc7ec755bcb22c2a13b134d1b88ef9b55e0
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-11-10 00:17:51 +0600

    search nav link

--- Nextjs/nextjs-13/nextjs-13-demo/app/Header.tsx
+++ Nextjs/nextjs-13/nextjs-13-demo/app/Header.tsx
@@ -5,6 +5,7 @@ export default function Header() {
       <header className="bg-black p-5 mb-5 flex justify-start gap-6">
          <Link href="/" className="text-white">Home</Link>
          <Link href="/todos" className="text-white">Todo</Link>
+         <Link href="/search" className="text-white">Search</Link>
       </header>
    );
 };
