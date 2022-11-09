3172b4e53b29d4ec13a0717842409a10ccdae104
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-11-09 21:43:42 +0600

    static generation for first 10 todos pages. revalidate cache after 60 seconds. Add not found 404 page

--- Nextjs/nextjs-13/nextjs-13-demo/app/todos/[id]/page.tsx
+++ Nextjs/nextjs-13/nextjs-13-demo/app/todos/[id]/page.tsx
@@ -1,4 +1,7 @@
 import { Todo } from "../../../typings";
+import { notFound } from "next/navigation";
+
+export const dynamicParams = true;
 
 type PageProps = {
    params: {
@@ -7,7 +10,7 @@ type PageProps = {
 };
 
 const fetchTodoById = async (id: string) => {
-   const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+   const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, { next: { revalidate: 60 } });
    const todo: Todo = await response.json();
 
    return todo;
@@ -16,6 +19,10 @@ const fetchTodoById = async (id: string) => {
 export default async function TodoPage({ params: { id } }: PageProps) {
    const todo = await fetchTodoById(id);
 
+   if(!todo.id) {
+      return notFound();
+   }
+
    return (
       <div className="bg-yellow-200 p-10">
          <p>Todo #{todo.id}: {todo.title}</p>
@@ -23,4 +30,16 @@ export default async function TodoPage({ params: { id } }: PageProps) {
          <p className="border-t border-black mt-5">By User: {todo.userId}</p>
       </div>
    );
+};
+
+// generate static site pages for first 10 todos
+export async function generateStaticParams() {
+   const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
+   const todos: Todo[] = await response.json();
+
+   const trimmedTodos = todos.splice(0, 10); // first ten todos
+
+   return trimmedTodos.map((todo) => ({
+      id: todo.id.toString()
+   }));
 };
