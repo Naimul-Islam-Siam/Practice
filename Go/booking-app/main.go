dd79d0d6a4bd8d7a80b550fe5d164c898f1e2171
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-11-15 20:21:07 +0600

    for, if, else

--- Go/booking-app/main.go
+++ Go/booking-app/main.go
@@ -1,11 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	const eventName string = "Go Conference, 2022"
 	const totalTickets uint8 = 50
-	var remainingTickets uint8 = 30
+	const reservedTickets uint8 = 10
+	var remainingTickets uint8 = totalTickets - reservedTickets
+	var bookings []string
+
 	fmt.Println("Welcome to", eventName, "booking")
 	fmt.Printf("Only %v tickets left\n", remainingTickets)
 	fmt.Println("Hurry up! Get your tickets now!")
@@ -15,19 +21,42 @@ func main() {
 	var email string
 	var userTickets uint8
 
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+	for {
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter your email address: ")
-	fmt.Scan(&email)
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
 
-	fmt.Println("How many tickets do you want: ")
-	fmt.Scan(&userTickets)
+		fmt.Println("How many tickets do you want: ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
+		if remainingTickets >= userTickets {
+			bookings = append(bookings, firstName+" "+lastName)
+			remainingTickets = remainingTickets - userTickets
 
-	fmt.Printf("Congratulations %v %v. You bought %v tickets. A confirmation mail will be sent to %v.", firstName, lastName, userTickets, email)
+			fmt.Printf("Congratulations %v %v. You bought %v tickets. A confirmation mail will be sent to %v.\n", firstName, lastName, userTickets, email)
+
+			var firstNamesofUsers []string
+
+			for _, booking := range bookings {
+				var extractedFirstname = strings.Fields(booking)[0]
+				firstNamesofUsers = append(firstNamesofUsers, extractedFirstname)
+			}
+
+			fmt.Printf("%v tickets left.\n People who booked: %v.\n", remainingTickets, firstNamesofUsers)
+
+			var noTicketsRemaining bool = remainingTickets == 0
+
+			if noTicketsRemaining {
+				fmt.Println("Tickets sold out. Hope to see you next year!")
+				break
+			}
+		} else {
+			fmt.Printf("Sorry! process failed, we only have %v tickets left.\n", remainingTickets)
+		}
+	}
 }
